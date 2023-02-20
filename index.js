2c452a63d85a9973f994844faaffb3aaf381c25a
Stefan Marinov <stefan_marinov@rogers.com>
2023-02-20 13:46:32 -0500

    code refactoring database.js to include all operations in the class

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const CLI = require('./lib/cli.js');
+const CLI = require('./lib/cli');
+const DB = require('./lib/db');
+
 
 const cli = new CLI();
+const db = new DB(cli).connectToDb();
 
-cli.run();
